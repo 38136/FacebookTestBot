50e37a608016b588632ccad0e32b23f98a39d198
38136 <38136.hexaware@gmail.com>
2017-11-24 09:15:23 +0530

    second commit

--- index.js
+++ index.js
@@ -1,94 +1,119 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app  = express();
+//index root
+app.set('port',(process.env.PORT||5000))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req,res){
+    res.send("Hello world, I am a chat bot");
+});
+
+
+//For facebook verification
+app.get('/webhook/', function(res, req){
+    if(req.query[hub.verify_token]==="test_bot"){
+        res.send(req.query['hub.challange'])
+    }
+    res.send("error,wrong token");
+});
+
+// //spin up the server
+app.listen(app.get('port'), function(){
+    console.log('running on the port : 5000');
+});
+// -------------------------------------------
+
 // const express = require('express');
 // const bodyParser = require('body-parser');
+// const fbgraph = require('fbgraphapi');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
 
-// const app  = express();
+// const app = express();
 // //index root
-// app.set('port',(process.env.PORT||5000))
+// app.set('port', (process.env.PORT || 5000))
 // app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+// app.use(cookieParser());
 
-// app.get('/', function(req,res){
+// app.get('/webhook', function (req, res) {
+//     if (req.query['hub.mode'] === 'subscribe' &&
+//         req.query['hub.verify_token'] === 'abcd1234') {
+//         console.log("Validating webhook");
+//         res.status(200).send(req.query['hub.challenge']);
+//     } else {
+//         console.error("Failed validation. Make sure the validation tokens match.");
+//         res.sendStatus(403);
+//     }
+// });
+
+// app.use(fbgraph.auth({
+//     appId: "2021811441383791",
+//     appSecret: "1d3a7e4893114a717eb2792a13035a6b",
+//     redirectUri: "https://www.facebook.com/Flight_Link-477957565916748/",
+//     apiVersion: "v2.9",
+//     skipUrlPatterns: ["/favicon.ico"]
+// }));
+
+// // let accessToken = 
+// // var fb = new fbgraph.Facebook(accessToken, 'v2.2');
+// // fb.me(function(err, me) {
+// //  console.log(me);
+// // });
+
+// app.get('/login', function (req, res) {
+//     console.log('Start login');
+//     fbgraph.redirectLoginForm(req, res);
+// });
+
+// app.get('/', function (req, res) {
+//     if (!req.hasOwnProperty('facebook')) {
+//         console.log('You are not logged in');
+//         return res.redirect('/login');
+//     }
+//     /* See http://developers.facebook.com/docs/reference/api/ for more */
+//     req.facebook.graph('/me', function (err, me) {
+//         console.log(me);
+//     });
+
+//     req.facebook.graph('/me?fields=id,name', function (err, me) {
+//         console.log(me);
+//     });
+
+//     req.facebook.me(function (err, me) {
+//         console.log(me);
+//     });
+
+//     // /me/likes
+//     req.facebook.my.likes(function (err, likes) {
+//         console.log(likes);
+//     });
+
+//     res.end("Check console output");
+// });
+
+// app.get('/', function (req, res) {
 //     res.send("Hello world, I am a chat bot");
 // });
 
 
 // //For facebook verification
-// app.get('/webhook/', function(res, req){
-//     if(req.query[hub.verify_token]==="test_bot"){
-//         res.send(req.query['hub.challange'])
-//     }
-//     res.send("error,wrong token");
-// });
+// // app.get('/webhook/', function (res, req) {
+// //     if (req.query[hub.verify_token] === "abcd1234") {
+// //         res.send(req.query['hub.challange'])
+// //     }
+// //     res.send("error,wrong token");
+// // });
 
 // // //spin up the server
-// app.listen(app.get('port'), function(){
+// app.listen(app.get('port'), function () {
 //     console.log('running on the port : 5000');
 // });
+// console.log("hai");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'abcd1234') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
-app.post('/webhook/', function (req, res) {
-    console.log("getting into the post");
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-             console.log("getting into the post 2");
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})
-
-const token = "EAAcu06jEOW8BAKaHyGKQwXv5h3jQjBYf80a0JL1at0ZAabK8J1SrzHoE8YDaWlIPU1wdjv2WBImauHtCD3OxSpMTjEv4fO8mPxi4JRHefkYZAZAPWIyrafUDnB1lDcZCwrEwSPfZAvaxdmMY7KGZAWspGvIWOZBJHwQAZCIZCM9p7IzkNP1OmlZCZBx"
-function sendTextMessage(sender, text) {
-    let messageData = { text:hi }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: "hi",
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
+// ------------------------------------------------------
---
+++ try22.js
@@ -0,0 +1,218 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+// imports node_cron for scheduled messaging
+var cron = require('node-cron');
+// sets variable for loading and parsing JSON
+var fs = require('fs');
+var PAGE_ACCESS_TOKEN = 'EAAcu06jEOW8BAKck0ssyS9yRtxE8WBQr0pt7f6Sh3NZAgecC7axYfctUnUEFap8mj1mw9sW13K5qh6OIsRwZC9Llo3BhUQFWRIE1EbqB6eUDeThtHz9NOrHZCWRVb4NvnP1c5hO8DNVKY9nZCW0ZBpTsRTdXCgDChUJQ6HXEmhgCufb26D05U';
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+    response.render('pages/index');
+});
+
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'abcd1234') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+app.post('/webhook', function (req, res) {
+    console.log('Message received');
+    //res.status(200).send();
+    //return;
+
+    var data = req.body;
+    console.log('Message received 2');
+
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function (entry) {
+            var pageID = entry.id;
+            var timeOfEvent = entry.time;
+            console.log('Message received 3');
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function (event) {
+                if (event.message) {
+                    receivedMessage(event);
+                    console.log('Message received 4');
+                } else {
+                    console.log("Webhook received unknown event: ", event);
+                    console.log('Message received 5');
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200);
+    }
+});
+
+// function receivedMessage(event) {
+//   // Putting a stub for now, we'll expand it in the following steps
+//   console.log("Message data: ", event.message);
+// }
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+
+    if (messageText) {
+
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
+
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
+    }
+}
+
+function sendGenericMessage(recipientId) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [{
+                        title: "rift",
+                        subtitle: "Next-generation virtual reality",
+                        item_url: "https://www.oculus.com/en-us/rift/",
+                        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                        buttons: [{
+                            type: "web_url",
+                            url: "https://www.oculus.com/en-us/rift/",
+                            title: "Open Web URL"
+                        }, {
+                            type: "postback",
+                            title: "Call Postback",
+                            payload: "Payload for first bubble",
+                        }],
+                    }, {
+                        title: "touch",
+                        subtitle: "Your Hands, Now in VR",
+                        item_url: "https://www.oculus.com/en-us/touch/",
+                        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                        buttons: [{
+                            type: "web_url",
+                            url: "https://www.oculus.com/en-us/touch/",
+                            title: "Open Web URL"
+                        }, {
+                            type: "postback",
+                            title: "Call Postback",
+                            payload: "Payload for second bubble",
+                        }]
+                    }]
+                 }
+            }
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function sendTextMessage(recipientId, messageText) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: content
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN
+        },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s",
+                messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            console.error(response);
+            console.error(error);
+        }
+    });
+}
+
+function test() {
+    // var pairs = [
+    //             { entity: "blah", sentiment: "blah"},
+    //             { entity: "blah", sentiment: "blah"},
+    //             { entity: "blah", sentiment: "blah"}
+    // ];
+    var content = JSON.parse(fs.readFileSync(__dirname + "data.json", "utf8"));
+    // for (var )
+}
+
+// cron.schedule('0 8 * * *', function(){
+//   //json parsing happens here;
+// });
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
